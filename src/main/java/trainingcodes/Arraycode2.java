6c45036e78c0944cfee9a7fdeb415e3c1511d211
AkshataPatil24 <128279198+AkshataPatil24@users.noreply.github.com>
2023-03-27 18:43:45 +0530

    Array Programs Added

---
+++ src/main/java/trainingcodes/Arraycode2.java
@@ -0,0 +1,32 @@
+package trainingcodes;
+import java.util.Scanner;
+public class Arraycode2 {
+
+	
+	public static void main(String[] args) {
+	 System.out.println("enter the count of classes:");
+				Scanner sc=new Scanner(System.in);
+				int cls=sc.nextInt();
+				System.out.println("enter the count of students in each class:");
+				int stu=sc.nextInt();
+				int arr[][]=new int[cls][stu];
+				for(int i=0;i<cls;i++) {
+					System.out.println("inside the cls"+(i+1));
+					for(int j=0;j<stu;j++) {
+						System.out.println("enter the marks of student"+(j+1));
+						arr[i][j]=sc.nextInt();
+					}
+				}
+				System.out.println("----------");
+				for(int i=0;i<cls;i++) {
+					System.out.println("inside the cls"+(i+1));
+					for(int j=0;j<stu;j++) {
+					System.out.println("the marks of student "+(j+1)+"is:"+arr[i][j]);
+				
+					}
+				}
+				
+			}
+			
+
+}
